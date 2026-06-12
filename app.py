c62c34958911c5b49cf3da0abfef8a99f8e3f76b
GabrielGarrido123 <gabriel.garrido@ug.uchile.cl>
2026-06-12 18:41:40 -0400

    0.1
    Version completamente en blanco, que lo unico que hace es abrir una ventana.

---
+++ app.py
@@ -0,0 +1,87 @@
+#Librerias externas
+from pyglet.graphics.shader import Shader, ShaderProgram
+from pyglet.window import Window, key
+from pyglet.gl import *
+from pyglet.app import run
+from pyglet import math
+from pyglet import clock
+from pyglet import text
+
+import numpy as np #recordar que en general usamos numpy 2.3.5
+import trimesh as tm
+
+#Librerias Oficiales de Python
+import sys, os
+import math as mathPY
+
+#Codigos del curso CC3501
+from utils.helpers import init_pipeline, mesh_from_file, init_axis
+from utils.camera import FreeCamera
+from utils.scene_graph import SceneGraph
+from utils.drawables import Texture, Model, DirectionalLight, Material
+from utils import shapes
+
+class Controller(Window):
+    #hereda caracteristicas de pyglet.window.Window
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args,**kwargs)
+
+        self.time = 0
+
+        self.wireframe = False
+        self.mouseLocked = True
+
+class MyCam(FreeCamera):
+    #hereda caracteristicas de utils.camera.FreeCamera
+    def __init__(self, position=np.array([0,0,0]), camera_type="perspective", direction=np.array([0,0,0]), speed=2):
+        super().__init__(position, camera_type)
+        self.direction = direction
+        self.speed = speed
+    
+    def time_update(self,dt):
+        self.update() #metodo update() heredado de FreeCamera
+        dir = self.direction[0]*self.forward + self.direction[1]*self.right
+        dir_norm = np.linalg.norm(dir)
+        if dir_norm:
+            dir /= dir_norm
+        self.position += dir*self.speed*dt
+        self.focus = self.position + self.forward
+
+if __name__ == "__main__":
+    #Crear la ventana
+    controller = Controller(800,600, "CraftMine")
+    controller.set_exclusive_mouse(controller.mouseLocked)
+
+    cam = MyCam([5,5,5])
+
+    world = SceneGraph(cam)
+
+    @controller.event
+    def on_draw():
+        controller.clear()
+
+        glClearColor(0.2,0.2,0.2,1)
+        glEnable(GL_DEPTH_TEST)
+
+        if controller.wireframe:
+            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
+        else:
+            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
+        
+        world.draw()
+    
+    @controller.event
+    def on_key_press(symbol, modifiers):
+        if symbol == key.M:
+            #Accion para poder recuperar el mouse y que no este anclado al programa (sin tener que cerrarlo o apretar la tecla Super para esto)
+            controller.mouseLocked = not controller.mouseLocked
+            controller.set_exclusive_mouse(controller.mouseLocked)
+
+    def update(dt):
+        world.update()
+        cam.time_update(dt)
+
+        controller.time += dt
+    
+    clock.schedule_interval(update, 1/60)
+    run()
